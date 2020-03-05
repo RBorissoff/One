f5d27b9dc55c9d36cbf0ddd8e3980840958ac72b
Borissoff <vwjetta808@gmail.com>
2020-03-06 00:36:35 +0300

    added realization of LoginPage check

--- conftest.py
+++ conftest.py
@@ -5,9 +5,9 @@ from selenium.webdriver.chrome.options import Options
 
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default='chrome', \
-                                help='\nChoose browser: chrome or firefox\n')
+                    help='\nChoose browser: chrome or firefox\n')
     parser.addoption('--language', action='store',default=None,\
-                                help="Choose language: ru, en, ... (etc.)")
+                    help="Choose language: ru, en, ... (etc.)")
 
 @pytest.fixture(scope="function")
 def browser(request):
